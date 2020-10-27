76ddb58c2b3d3b43ca17da015cea1d86217542a7
Ryan <rb222rx@student.lnu.se>
2020-10-27 11:15:58 +0100

    push 4real

--- BlackJack/model/Dealer.java
+++ BlackJack/model/Dealer.java
@@ -7,25 +7,27 @@ public class Dealer extends Player {
   private Deck m_deck;
   private INewGameStrategy m_newGameRule;
   private IHitStrategy m_hitRule;
+  private IWinStrategy winStrat;
 
   public Dealer(RulesFactory a_rulesFactory) {
-  
+
     m_newGameRule = a_rulesFactory.GetNewGameRule();
     m_hitRule = a_rulesFactory.GetHitRule();
-    
+    winStrat = a_rulesFactory.getWinner();
+
     /*for(Card c : m_deck.GetCards()) {
       c.Show(true);
       System.out.println("" + c.GetValue() + " of " + c.GetColor());
     }    */
   }
-  
-  
+
+
   public boolean NewGame(Player a_player) {
     if (m_deck == null || IsGameOver()) {
       m_deck = new Deck();
       ClearHand();
       a_player.ClearHand();
-      return m_newGameRule.NewGame(m_deck, this, a_player);   
+      return m_newGameRule.NewGame(m_deck, this, a_player);
     }
     return false;
   }
@@ -36,24 +38,19 @@ public class Dealer extends Player {
       c = m_deck.GetCard();
       c.Show(true);
       a_player.DealCard(c);
-      
+
       return true;
     }
     return false;
   }
 
   public boolean IsDealerWinner(Player a_player) {
-    if (a_player.CalcScore() > g_maxScore) {
-      return true;
-    } else if (CalcScore() > g_maxScore) {
-      return false;
-    }
-    return CalcScore() >= a_player.CalcScore();
+    return winStrat.DoWin(CalcScore(), a_player.CalcScore(), g_maxScore);
   }
 
   public boolean IsGameOver() {
     if (m_deck != null && m_hitRule.DoHit(this) != true) {
-        return true;
+      return true;
     }
     return false;
   }
