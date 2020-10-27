6b395e19eb48d1bbecc4976d78a1af1117b004f0
cn22tm <cn222tm@student.lnu.se>
2020-10-27 16:28:10 +0100

    push

--- BlackJack/controller/Observer.java
+++ BlackJack/controller/Observer.java
@@ -1,30 +1,5 @@
 package BlackJack.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-class EventSource {
-
-    public interface Observer {
-        void update(String event);
-    }
-
-    final List<Observer> observers = new ArrayList<>();
-
-    private void notifyObservers(String event) {
-        observers.forEach(observer -> observer.update(event)); //alternative lambda expression: observers.forEach(Observer::update);
-    }
-
-    public void addObserver(Observer observer) {
-        observers.add(observer);
-    }
-
-    public void scanSystemIn() {
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            notifyObservers(line);
-        }
-    }
+public interface Observer {
+    void update(Object o);
 }
