c986aa85871bab6a61eac8c3acf1a3e8e2aa5a11
RyanBui99 <65392753+RyanBui99@users.noreply.github.com>
2020-10-27 11:06:13 +0100

    Update PlayGame.java

--- BlackJack/controller/PlayGame.java
+++ BlackJack/controller/PlayGame.java
@@ -3,7 +3,7 @@ package BlackJack.controller;
 import BlackJack.view.IView;
 import BlackJack.model.Game;
 
-public class PlayGame {
+public class PlayGame implements Observer{
 
   public boolean Play(Game a_game, IView a_view) {
     a_view.DisplayWelcomeMessage();
@@ -16,10 +16,21 @@ public class PlayGame {
         a_view.DisplayGameOver(a_game.IsDealerWinner());
     }
 
-    int input = a_view.GetInput();
-    
+    IView.InputMenu input = a_view.GetInput();
 
+    if (input == IView.InputMenu.p) {
+      a_game.NewGame();
+    } else if (input == IView.InputMenu.h) {
+      a_game.Hit();
+    } else if (input == IView.InputMenu.s) {
+      a_game.Stand();
+    }
 
-    return input != 'q';
+    return input != IView.InputMenu.q;
+  }
+
+  @Override
+  public void update() {
+
   }
 }
